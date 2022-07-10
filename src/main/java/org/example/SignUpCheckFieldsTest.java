677cb9df6cd7793e3aad1f0696cb1407fac2fce2
Alex Sudarikov <galee.91@gmail.com>
2022-07-11 03:46:03 +0400

    Initial commit

---
+++ src/main/java/org/example/SignUpCheckFieldsTest.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SignUpCheckFieldsTest {
+    @Test
+    public void CheckInputField() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); /*просетать в системные пропети хромдрайвер и определить путь к нему*/
+        WebDriver driver = new ChromeDriver(); /*ОБъявить переменную вебдрайвер для последующейго взаимодействия с браузером хром*/
+
+        /*Тест на наличие полей ввода  First Name, Last Name, Email, Password, Confirm Password и на регистрацию при валидных значениях*/
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");/* Переходим на SignUp page*/
+        assertTrue(driver.findElement(By.name("first_name")).isDisplayed(), "Отсутствует Элемент first_name ");
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        assertTrue(driver.findElement(By.name("last_name")).isDisplayed(), "Отсутствует Элемент last_name ");
+        driver.findElement(By.name("last_name")).sendKeys("Sudarikov");
+        assertTrue(driver.findElement(By.name("email")).isDisplayed(), "Отсутствует Элемент email ");
+        driver.findElement(By.name("email")).sendKeys("fdser@gmail.com");
+        assertTrue(driver.findElement(By.name("password1")).isDisplayed(), "Отсутствует Элемент Password ");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        assertTrue(driver.findElement(By.name("password2")).isDisplayed(), "Отсутствует Элемент Confirm Password ");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "Отсутствует Элемент ");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.cssSelector("span[class='confirmation_message']")).isDisplayed(), "перепиши тест");
+
+        //Тест на невалидные данные в поле First Name(набор символов)
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        assertTrue(driver.findElement(By.name("first_name")).isDisplayed(), "Отсутствует Элемент first_name ");
+        driver.findElement(By.name("first_name")).sendKeys("!@#$%$#@@");
+        assertTrue(driver.findElement(By.name("last_name")).isDisplayed(), "Отсутствует Элемент last_name ");
+        driver.findElement(By.name("last_name")).sendKeys("Sudarikov");
+        assertTrue(driver.findElement(By.name("email")).isDisplayed(), "Отсутствует Элемент email ");
+        driver.findElement(By.name("email")).sendKeys("fdser@gmail.com");
+        assertTrue(driver.findElement(By.name("password1")).isDisplayed(), "Отсутствует Элемент Password ");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        assertTrue(driver.findElement(By.name("password2")).isDisplayed(), "Отсутствует Элемент Confirm Password ");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "Отсутствует Элемент ");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");
+
+        //Тест на невалидные данные в поле Email(ввести емэйл без точки)
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        assertTrue(driver.findElement(By.name("first_name")).isDisplayed(), "Отсутствует Элемент first_name ");
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        assertTrue(driver.findElement(By.name("last_name")).isDisplayed(), "Отсутствует Элемент last_name ");
+        driver.findElement(By.name("last_name")).sendKeys("");
+        assertTrue(driver.findElement(By.name("email")).isDisplayed(), "Отсутствует Элемент email ");
+        driver.findElement(By.name("email")).sendKeys("fdser@gmailcom");
+        assertTrue(driver.findElement(By.name("password1")).isDisplayed(), "Отсутствует Элемент Password ");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        assertTrue(driver.findElement(By.name("password2")).isDisplayed(), "Отсутствует Элемент Confirm Password ");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "Отсутствует Элемент ");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");
+
+        //Тест на невалидные данные в поле Password(оставить его пустым)
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        assertTrue(driver.findElement(By.name("first_name")).isDisplayed(), "Отсутствует Элемент first_name ");
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        assertTrue(driver.findElement(By.name("last_name")).isDisplayed(), "Отсутствует Элемент last_name ");
+        driver.findElement(By.name("last_name")).sendKeys("");
+        assertTrue(driver.findElement(By.name("email")).isDisplayed(), "Отсутствует Элемент email ");
+        driver.findElement(By.name("email")).sendKeys("fdser@gmail.com");
+        assertTrue(driver.findElement(By.name("password1")).isDisplayed(), "Отсутствует Элемент Password ");
+        driver.findElement(By.name("password1")).sendKeys("");
+        assertTrue(driver.findElement(By.name("password2")).isDisplayed(), "Отсутствует Элемент Confirm Password ");
+        driver.findElement(By.name("password2")).sendKeys("1111");
+        registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "Отсутствует Элемент ");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");
+
+        //Тест на невалидные данные в поле Confirm Password(оставить его пустым)
+
+        driver.get("https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=12345");
+        assertTrue(driver.findElement(By.name("first_name")).isDisplayed(), "Отсутствует Элемент first_name ");
+        driver.findElement(By.name("first_name")).sendKeys("Alex");
+        assertTrue(driver.findElement(By.name("last_name")).isDisplayed(), "Отсутствует Элемент last_name ");
+        driver.findElement(By.name("last_name")).sendKeys("");
+        assertTrue(driver.findElement(By.name("email")).isDisplayed(), "Отсутствует Элемент email ");
+        driver.findElement(By.name("email")).sendKeys("fdser@gmailcom");
+        assertTrue(driver.findElement(By.name("password1")).isDisplayed(), "Отсутствует Элемент Password ");
+        driver.findElement(By.name("password1")).sendKeys("1111");
+        assertTrue(driver.findElement(By.name("password2")).isDisplayed(), "Отсутствует Элемент Confirm Password ");
+        driver.findElement(By.name("password2")).sendKeys("");
+        registerButton = driver.findElement(By.cssSelector("input[value='Register']"));
+        assertTrue(registerButton.isDisplayed(), "Отсутствует Элемент ");
+        driver.findElement(By.cssSelector("input[value='Register']")).click();
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");
+
+
+
+    }
+}
---
+++ src/main/java/org/example/SignUpTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class SignUpTest {
+
+    /*
+    1. Open browser
+    2. go to https://www.sharelane.com/cgi-bin/register.py
+    3. input numbers 12345
+    4. Press "Continue button"
+     */
+
+    @Test
+    public void ZipCodeShouldBeValid() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); /*просетать в системные пропети хромдрайвер и определить путь к нему*/
+        WebDriver driver = new ChromeDriver(); /*ОБъявить переменную вебдрайвер для последующейго взаимодействия с браузером хром*/
+
+        /*Тест на ошибку при пустом поле ввода*/
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");/* Переходим на zip code page*/
+        driver.findElement(By.name("zip_code")).sendKeys("");/*Ищем элемент зип код и вводим в это поле пустое значение*/
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();/* после ввода зип кода найти элемент континуе и кликнуть по нему*/
+        /*проверка что невалидное значение привело к ожидаемой ошибке*/
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");/* после ввода зип кода найти элемент континуе и кликнуть по нему*/
+
+        /*Второй тест на валидное значение 12345 в поле зип код*/
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");/* Переходим на zip code page*/
+        driver.findElement(By.name("zip_code")).sendKeys("12345");/*Ищем элемент зип код и вводим в это поле значение 12345*/
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();/* после ввода зип кода найти элемент континуе и кликнуть по нему*/
+        /*проверка что валидное значение принято*/
+        WebElement registerButton = driver.findElement(By.cssSelector("input[value='Register']"));/*На след странице объявить переменную регистербаттон и найти ее на странице*/
+        assertTrue(registerButton.isDisplayed(), "User LOH");/*Убедиться что элемент регистербаттон существует и если нет то вывести сообщение юзеру*/
+
+        /*Тест на ошибку при xss инъекции*/
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");/* Переходим на zip code page*/
+        driver.findElement(By.name("zip_code")).sendKeys("<script>alert('XSS');</script>");/*Ищем элемент зип код и вводим в это поле инъекцию*/
+        driver.findElement(By.cssSelector("input[value='Continue']")).click();/* после ввода зип кода найти элемент континуе и кликнуть по нему*/
+        /*проверка что невалидное значение привело к ожидаемой ошибке*/
+        assertTrue(driver.findElement(By.cssSelector("span[class='error_message']")).isDisplayed(), "перепиши тест");/* после ввода зип кода найти элемент континуе и кликнуть по нему*/
+
+
+
+    }
+}
