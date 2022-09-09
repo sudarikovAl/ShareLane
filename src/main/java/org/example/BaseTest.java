3bb90c1fb8dae036193d5d99e62799d574e19a2e
Alex Sudarikov <galee.91@gmail.com>
2022-09-09 05:21:19 +0400

    Sudarikov_Lesson6_Changes_after_watching_the_video

--- src/main/java/org/example/BaseTest.java
+++ src/main/java/org/example/BaseTest.java
@@ -1,23 +1,24 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
 
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeClass
-    public void setPathToWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-    }
-
     @BeforeMethod
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        System.out.println();
     }
 
     @AfterMethod(alwaysRun = true)
--- src/main/java/org/example/SignUpCheckFieldsTest.java
+++ src/main/java/org/example/SignUpCheckFieldsTest.java
@@ -1,10 +1,7 @@
 package org.example;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertTrue;
